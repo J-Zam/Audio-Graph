c48c4f597e112bad5fbb364209da334298cd72a8
unknown <zambranojunnior069@gmail.com>
2022-11-24 16:30:21 -0600

    fix: some issues solved

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,23 @@
 import "./style.scss";
-import { rocketMan } from "./base64/rocketMan";
 
 let analyser: AnalyserNode;
 let audioSource: MediaElementAudioSourceNode;
 const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
 const file = document.getElementById("fileupload") as HTMLInputElement;
+const loading = document.getElementById("loading") as HTMLElement;
 const audioPlayer = document.getElementById("audioPlayer") as HTMLAudioElement;
-audioPlayer.src = rocketMan;
-const FFTSIZE = 512; // 32, 64, 128, 256, 512, 1024, 2048
+const inputWrapper = document.getElementById("inputWrapper") as HTMLAudioElement;
+type fftSizeType = 32 | 64 | 128 | 256 | 512 | 1024 | 2048;
+const FFTSIZE: fftSizeType = 512;
 const lINEWIDTH = 2;
 
-window.addEventListener("DOMContentLoaded", () => {
+window.addEventListener("DOMContentLoaded", async () => {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
+  let rocketMan = await import("./base64/rocketMan");
+  audioPlayer.src = await rocketMan.rocketMan;
+  loading.style.display = "none";
 });
 
 window.addEventListener("resize", () => {
@@ -31,17 +35,19 @@ audioPlayer.addEventListener("play", function () {
   analyser.fftSize = FFTSIZE;
   const bufferLength = analyser.frequencyBinCount;
   const dataArray = new Uint8Array(bufferLength);
-
   const lineWidth = lINEWIDTH;
-  let barHeigth: number;
 
   function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     analyser.getByteFrequencyData(dataArray);
-    drawVisualizer(bufferLength, lineWidth, barHeigth, dataArray);
+    drawVisualizer(bufferLength, lineWidth, dataArray);
     requestAnimationFrame(animate);
   }
   animate();
+
+  setInterval(() => {
+    inputWrapper.classList.add("active");
+  }, 700);
 });
 
 file.addEventListener("change", function () {
@@ -58,14 +64,12 @@ file.addEventListener("change", function () {
   analyser.fftSize = FFTSIZE;
   const bufferLength = analyser.frequencyBinCount;
   const dataArray = new Uint8Array(bufferLength);
-
   const lineWidth = lINEWIDTH;
-  let barHeigth: number;
 
   function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     analyser.getByteFrequencyData(dataArray);
-    drawVisualizer(bufferLength, lineWidth, barHeigth, dataArray);
+    drawVisualizer(bufferLength, lineWidth, dataArray);
     requestAnimationFrame(animate);
   }
   animate();
@@ -74,16 +78,16 @@ file.addEventListener("change", function () {
 function drawVisualizer(
   bufferLength: number,
   lineWidth: number,
-  barHeigth: number,
   dataArray: Uint8Array
 ) {
   let hue = 0;
+  let barHeigth: number;
 
   for (let i = 0; i < bufferLength; i++) {
+    hue = i * 100;
     canvas.width > 700
       ? (barHeigth = dataArray[i] * 1.5)
       : (barHeigth = dataArray[i]);
-    hue = i * 100;
 
     ctx.save();
     ctx.translate(canvas.width / 2, canvas.height / 2);
@@ -94,7 +98,6 @@ function drawVisualizer(
     ctx.beginPath();
     ctx.arc(0, barHeigth, barHeigth / 13, 0, Math.PI * 2);
     ctx.fill();
-
     ctx.restore();
   }
 }
